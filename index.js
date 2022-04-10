92fbfb5b951965024c12f3c99c7e530229c76633
Jessica <jess.newman19@gmail.com>
2022-04-10 11:15:41 -0400

    commit tree ba8d1317b4fcb9acd2e7178d2ff671ed78408a41

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+  let id = document.querySelector("#text"); 
+  // console.log(id); returns <p>
+  id.textContent = "This is really cool!"
+  // console.log(id); returns <p> with new text inside
+}
+)
